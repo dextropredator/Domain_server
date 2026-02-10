b0f1b0eafa9841706f9a299c9dba67d431ccf794
dextropredator <kumar292715@gmail.com>
2026-02-10 10:57:30 +0530

    first commit

--- Https.java
+++ Https.java
@@ -1,8 +1,13 @@
 import java.net.*;
 import java.io.*;
 public class Https {
+   public static void main(String[] args) throws Exception{
+    ServerSocket s = new ServerSocket(8080);
+Socket c = s.accept();
+c.getOutputStream().write("Hello dost\n".getBytes());
+c.close();;
+s.close();
 
-    public static void main(String[] args) {
-        System.out.println("hello helios");
-    }
+   }
+
 }
